f05c757287ecd8c344f137f64d31a5c2d9af18a0
C5281583 <thalesh.sharma@sap.com>
2020-07-09 19:36:15 +0530

    new class added : HelloWorld

---
+++ src/test/java/package1/HelloWorld.java
@@ -0,0 +1,13 @@
+package package1;
+
+import org.testng.annotations.Test;
+
+public class HelloWorld {
+
+	@Test
+	public void displayMessage() {
+		
+		System.out.println("Hellooooooooo World !!!!!!");
+
+	}
+}
