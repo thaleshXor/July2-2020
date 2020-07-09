0e5ec1ad72d5d8d704b87b194fb3158cdff9e000
C5281583 <thalesh.sharma@sap.com>
2020-07-09 20:30:12 +0530

    .

--- src/test/java/package1/HelloWorld.java
+++ src/test/java/package1/HelloWorld.java
@@ -8,6 +8,8 @@ public class HelloWorld {
 	public void displayMessage() {
 		
 		System.out.println("Hellooooooooo World !!!!!!");
-
+		String firstName=System.getenv("FIRSTNAME");
+		String secondName=System.getenv("LASTNAME");
+		System.out.println(">>>>>>>>>>>>" + firstName + "  " + secondName + "<<<<<<<<<<<<<<<<<<<");
 	}
 }
