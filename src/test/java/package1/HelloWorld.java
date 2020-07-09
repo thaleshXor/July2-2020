9c7f1bfafedd589ae36edf9964868813decb7b19
C5281583 <thalesh.sharma@sap.com>
2020-07-09 22:54:01 +0530

    conditional OS and browser

--- src/test/java/package1/HelloWorld.java
+++ src/test/java/package1/HelloWorld.java
@@ -9,7 +9,31 @@ public class HelloWorld {
 		
 		System.out.println("Hellooooooooo World !!!!!!");
 		String firstName=System.getenv("FIRSTNAME");
-		String secondName=System.getenv("LASTNAME");
+		String secondName=System.getenv("SURNAME");
 		System.out.println(">>>>>>>>>>>>" + firstName + "  " + secondName + "<<<<<<<<<<<<<<<<<<<");
+		
+		
+		String osName=System.getProperty("os.name");
+		String browser=System.getenv("BROWSER");
+		
+		if(osName.toUpperCase().contains("WINDOWS")) {
+			
+			System.out.println("*************I am running on WINDOWS **************");
+		}
+		else if(osName.toUpperCase().contains("LINUX")) {
+			
+			System.out.println("*************I am running on LINUX **************");
+		}
+		
+		
+		if(browser.toUpperCase().contains("CHROME")) {
+			
+			System.out.println("*************I am running on CHROME **************");
+		}
+		else if(osName.toUpperCase().contains("FIREFOX")) {
+			
+			System.out.println("*************I am running on FIREFOX **************");
+		}		
+		
 	}
 }
