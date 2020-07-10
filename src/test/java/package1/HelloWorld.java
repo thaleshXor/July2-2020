bdb67aad8c973fe364a4ee12a2d7d2ec13a83c73
thaleshXor <thalesh.sharma@sap.com>
2020-07-10 23:33:43 +0530

    files changed

--- src/test/java/package1/HelloWorld.java
+++ src/test/java/package1/HelloWorld.java
@@ -3,22 +3,24 @@ package package1;
 import org.testng.annotations.Test;
 
 public class HelloWorld {
-
+	String browser="";
 	@Test
 	public void displayMessage() {
 		
 		System.out.println("Hellooooooooo World !!!!!!");
 
 		String osName=System.getProperty("os.name");
-		String browser=System.getenv("BROWSER");
+		//String browser=System.getenv("BROWSER");
 		
 		if(osName.toUpperCase().contains("WINDOWS")) {
 			
 			System.out.println("*************I am running on WINDOWS **************");
+			browser=System.getenv("BROWSER");
 		}
 		else if(osName.toUpperCase().contains("LINUX")) {
 			
 			System.out.println("*************I am running on LINUX **************");
+			browser=System.getenv("BROWSER");
 		}
 		
 		
